f62c705996db1d3b2bce69a4b3b46b5503b59692
glenn.steven <glnsteven72@gmail.com>
2023-09-26 20:35:25 +0700

    add response and change relation table category book

--- controllers/book_controllers.go
+++ controllers/book_controllers.go
@@ -3,71 +3,113 @@ package controllers
 import (
 	"asis_quest/helper"
 	"asis_quest/models"
-	"asis_quest/repositories"
+	"asis_quest/presentation"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"net/http"
 	"time"
 )
 
-func CreateCategory(w http.ResponseWriter, r *http.Request) {
-	var category models.Categories
-
-	decoder := json.NewDecoder(r.Body)
-	err := decoder.Decode(&category)
-	if err != nil {
-		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	defer r.Body.Close()
-
-	category.CreatedAt = time.Now()
-	category.UpdatedAt = time.Now()
-	category.DeletedAt = nil
-
-	if err := models.DB.Create(&category).Error; err != nil {
-		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-
-	helper.ResponseJSON(w, http.StatusCreated, category)
-}
-
+// Book is get all data book from database
 func Book(w http.ResponseWriter, _ *http.Request) {
-	var book []models.Books
+	var (
+		book     []models.Books
+		response presentation.ResponseList
+	)
 
 	if err := models.DB.Find(&book).Error; err != nil {
 		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	helper.ResponseJSON(w, http.StatusOK, book)
+	for _, fetchBook := range book {
+		rupiah := helper.Accounting(fetchBook.Price)
+		resp := presentation.Data{
+			ID:          fetchBook.ID,
+			Title:       fetchBook.Title,
+			Description: fetchBook.Description,
+			Category:    nil,
+			Keyword:     fetchBook.Keyword,
+			Price:       rupiah,
+			Stock:       fetchBook.Stock,
+			Publisher:   fetchBook.Publisher,
+			CreatedAt:   fetchBook.CreatedAt,
+			UpdatedAt:   fetchBook.UpdatedAt,
+		}
+
+		for _, bc := range fetchBook.BookCategory {
+			var category models.Categories
+			if err := models.DB.Where("id = ?", bc.CategoryID).Find(&category).Error; err != nil {
+				helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+				return
+			}
+
+			resp.Category = append(resp.Category, category.Category)
+		}
+		response.Data = append(response.Data, resp)
+	}
+
+	response.Code = http.StatusOK
+	response.Message = "Fetch all books"
+	helper.ResponseJSON(w, http.StatusOK, response)
 }
 
 func CreateBook(w http.ResponseWriter, r *http.Request) {
 	var (
-		book     models.Books
-		response repositories.Response
+		book    models.Books
+		payload presentation.BookPayload
 	)
 
 	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&book); err != nil {
+
+	if err := decoder.Decode(&payload); err != nil {
 		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	defer r.Body.Close()
+	var existingBook models.Books
+	if err := models.DB.Where("title = ?", payload.Title).First(&existingBook).Error; err != nil {
+		response := map[string]string{"message": "data book already exist"}
+		helper.ResponseJSON(w, http.StatusConflict, response)
+		return
+	}
 
-	var exists bool
+	book = models.Books{
+		Title:       payload.Title,
+		Description: payload.Description,
+		Keyword:     payload.Keyword,
+		Price:       payload.Price,
+		Stock:       payload.Stock,
+		Publisher:   payload.Publisher,
+		CreatedAt:   time.Now().Format(`2006-01-02 15:04:05`),
+		UpdatedAt:   time.Now().Format(`2006-01-02 15:04:05`),
+	}
 
-	if err := models.DB.Where("title = ?", book.Title).Find(&book).Error; err != nil {
-		if !exists {
-			response := map[string]string{"message": "data book already exist"}
-			helper.ResponseJSON(w, http.StatusUnauthorized, response)
-			return
+	if err := models.DB.Create(&book).Error; err != nil {
+		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	for _, categoryName := range payload.Categories {
+		var category models.Categories
+		if err := models.DB.Where("category = ?", categoryName).First(&category).Error; err != nil {
+			category = models.Categories{
+				Category:  categoryName,
+				CreatedAt: time.Now().Format(`2006-01-02 15:04:05`),
+				UpdatedAt: time.Now().Format(`2006-01-02 15:04:05`),
+			}
+			if err := models.DB.Create(&category).Error; err != nil {
+				helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+				return
+			}
 		}
-	} else {
-		if err := models.DB.Create(&book).Error; err != nil {
+		bookCategory := models.BookCategory{
+			BookID:     book.ID,
+			CategoryID: category.ID,
+		}
+
+		if err := models.DB.Create(&bookCategory).Error; err != nil {
 			helper.ResponseError(w, http.StatusInternalServerError, err.Error())
 			return
 		}
@@ -75,20 +117,34 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 
 	rupiah := helper.Accounting(book.Price)
 
-	response.Code = http.StatusCreated
-	response.Message = "Successfully create new book"
-	response.Data.ID = book.Id
-	response.Data.CategoryID = book.CategoryID
-	response.Data.Title = book.Title
-	response.Data.Category = book.Category
-	response.Data.Keyword = book.Keyword
-	response.Data.Price = rupiah
-	response.Data.Stock = book.Stock
-	response.Data.Publisher = book.Publisher
-	response.Data.Description = book.Description
-	response.Data.CreatedAt = book.CreatedAt
-	response.Data.UpdatedAt = book.UpdatedAt
-	response.Data.DeletedAt = book.DeletedAt
+	response := presentation.Response{
+		Code:    http.StatusCreated,
+		Message: "Successfully create new book",
+		Data: presentation.Data{
+			ID:          book.ID,
+			Title:       book.Title,
+			Description: book.Description,
+			Keyword:     book.Keyword,
+			Price:       rupiah,
+			Stock:       book.Stock,
+			Publisher:   book.Publisher,
+			CreatedAt:   book.CreatedAt,
+			UpdatedAt:   book.UpdatedAt,
+		},
+	}
+
+	response.Data.Category = append(response.Data.Category, payload.Categories...)
 
 	helper.ResponseJSON(w, http.StatusCreated, response)
+}
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Books
+	bookID := mux.Vars(r)["book_id"]
+	if err := models.DB.Where("id = ?", bookID).First(&book).Error; err != nil {
+		helper.ResponseError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	helper.ResponseJSON(w, http.StatusOK, book)
 }
--- presentation/response_books.go
+++ presentation/response_books.go
@@ -1,4 +1,4 @@
-package repositories
+package presentation
 
 import "time"
 
@@ -10,15 +10,20 @@ type Response struct {
 
 type Data struct {
 	ID          int64      `json:"id"`
-	CategoryID  int64      `json:"category_id"`
 	Title       string     `json:"title"`
 	Description string     `json:"description"`
-	Category    string     `json:"category"`
+	Category    []string   `json:"category"`
 	Keyword     string     `json:"keyword"`
 	Price       string     `json:"price"`
 	Stock       int64      `json:"stock"`
 	Publisher   string     `json:"publisher"`
-	CreatedAt   time.Time  `json:"created_at"`
-	UpdatedAt   time.Time  `json:"updated_at"`
-	DeletedAt   *time.Time `json:"deleted_at"`
+	CreatedAt   string     `json:"created_at"`
+	UpdatedAt   string     `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at,omitempty"`
+}
+
+type ResponseList struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Data    []Data `json:"data"`
 }
